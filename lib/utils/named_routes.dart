46124136c3710b6f674905975de779a9c37e8f0e
vivek <Katariyavivek228@gmail.com>
2024-01-18 11:31:12 +0530

    final update

--- lib/utils/named_routes.dart
+++ lib/utils/named_routes.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:invoice_maker/screen/home/home_screen.dart';
 import 'package:invoice_maker/screen/invoice/invoice_view_screen.dart';
 import 'package:invoice_maker/screen/splesh/splesh_screen.dart';
+import 'package:invoice_maker/screen/user_input/user_input_screen.dart';
 
 Map<String,WidgetBuilder> routes={
   "/":(context) =>const SpleshScreen(),
   "home":(context) => const HomeScreen(),
+  "getter":(context) => const DetailGetter(),
   "invoice":(context) => const InvoiceView(),
 };
